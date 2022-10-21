d3244078f36bd890170797e593e8ae7ff739c2f0
Baka Cirno 999 <14835957@qq.com>
2022-10-21 19:46:44 +0800

    today

--- function.js
+++ function.js
@@ -1,13 +1,15 @@
 var vm = new Vue({
     el: '#vue_app',
     data: {
-        bg_desc: ""
+        bg_desc: "",
+        bg_more: ""
     },
     methods:{
         getArticles:function() {
             //发送get请求
             this.$http.get('https://bing.biturl.top/').then(function(res){
                 vm.bg_desc = res.body.copyright;
+                vm.bg_more = res.body.copyright_link;
             },function(){
                 console.log('Fail');
             });
